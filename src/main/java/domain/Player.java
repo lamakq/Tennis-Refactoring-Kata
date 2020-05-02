7c4cbecddce5ac9441c292b406dc52667547633b
abdalahmed <abdal.ahmed@careem.com>
2020-05-02 16:35:50 +0500

    Added Player Class

---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Player {
+
+    private String name;
+    private String score;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getScore() {
+        return score;
+    }
+}
