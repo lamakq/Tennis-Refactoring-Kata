801eba794f615ad46ee48b41f456f0b72cd073e9
Shakeel Hussain <shakeel.hussain@careem.com>
2020-05-02 15:49:38 +0500

    extracted score function cases , optimized equal score function

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -19,65 +19,78 @@ public class TennisGame1 implements TennisGame {
             player2Score += 1;
     }
 
+
+    public String getScore() {
+        String score = "";
+
+        if (areScoresEqual()) {
+            score = handleScoreEqualCase();
+        }
+        // winning and advantage cases
+        else if (isAdvantageOrWinningCase())
+        {
+            score = handleAdvantageCase();
+        }
+        else
+        {
+            score = handleNormalCase(score);
+        }
+        return score;
+    }
+
     private boolean areScoresEqual(){
         return player1Score == player2Score;
     }
 
+    private boolean isAdvantageOrWinningCase() {
+        return player1Score >=4 || player2Score >=4;
+    }
 
-    public String getScore() {
-        String score = "";
-        // scores are equal
-        if (areScoresEqual()) {
-            switch (player1Score)
+    private String handleNormalCase(String score) {
+        int tempScore=0;
+        for (int i=1; i<3; i++)
+        {
+            if (i==1) tempScore = player1Score;
+            else { score+="-"; tempScore = player2Score;}
+            switch(tempScore)
             {
                 case 0:
-                        score = "Love-All";
+                    score+="Love";
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                    score+="Fifteen";
                     break;
                 case 2:
-                        score = "Thirty-All";
+                    score+="Thirty";
                     break;
-                default:
-                        score = "Deuce";
+                case 3:
+                    score+="Forty";
                     break;
-
-            }
-        }
-        // winning and advantage cases
-        else if (player1Score >=4 || player2Score >=4)
-        {
-            int minusResult = player1Score - player2Score;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            int tempScore=0;
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = player1Score;
-                else { score+="-"; tempScore = player2Score;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
             }
         }
         return score;
+    }
+
+    private String handleAdvantageCase() {
+        String score;
+        int minusResult = player1Score - player2Score;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+
+    private String handleScoreEqualCase() {
+        switch (player1Score) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            default:
+                return "Deuce";
+        }
     }
 }
