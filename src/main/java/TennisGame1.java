2be37ab4fce561781a9de74df67869121178afed
faiq.muhammad <faiq.muhammad@careem.com>
2020-05-02 15:52:19 +0500

    get score string for default game state extracted default condition into method

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,6 +1,5 @@
-
 public class TennisGame1 implements TennisGame {
-    
+
     private int player1Score = 0;
     private int player2Score = 0;
     private String player1Name;
@@ -21,12 +20,15 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String getScoreWhenGameIsTied() {
-        switch (player1Score)
-        {
-            case 0:  return "Love-All";
-            case 1:  return "Fifteen-All";
-            case 2:  return "Thirty-All";
-            default: return "Deuce";
+        switch (player1Score) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            default:
+                return "Deuce";
         }
     }
 
@@ -34,58 +36,40 @@ public class TennisGame1 implements TennisGame {
         return player1Score >= 4 || player2Score >= 4;
     }
 
+    private String getScoreFromInteger(int score) {
+        switch (score) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            default:
+                return "Forty";
+        }
+    }
+
     private String getScoreWhenGameIsAtWinningOrAdvantageStatus() {
         int scoreDelta = player1Score - player2Score;
 
-        if (scoreDelta==1) {
+        if (scoreDelta == 1) {
             return "Advantage player1";
-        }
-        else if (scoreDelta ==-1) {
+        } else if (scoreDelta == -1) {
             return "Advantage player2";
-        }
-        else if (scoreDelta>=2) {
+        } else if (scoreDelta >= 2) {
             return "Win for player1";
-        }
-
-        else {
+        } else {
             return "Win for player2";
         }
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-
         if (isGameTied()) {
-            score = getScoreWhenGameIsTied();
+            return getScoreWhenGameIsTied();
+        } else if (isWinningOrAdvantageStatus()) {
+            return getScoreWhenGameIsAtWinningOrAdvantageStatus();
+        } else {
+            return String.format("%s-%s", getScoreFromInteger(player1Score), getScoreFromInteger(player2Score));
         }
-
-        else if (isWinningOrAdvantageStatus()) {
-            score = getScoreWhenGameIsAtWinningOrAdvantageStatus();
-        }
-
-        else {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = player1Score;
-                else { score+="-"; tempScore = player2Score;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
     }
 }
